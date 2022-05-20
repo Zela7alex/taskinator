8e5e728675fa488920a084bd92befb2684167ef8
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-05-20 00:58:55 -0400

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,37 @@
 const tasksToDoEl = document.querySelector('#tasks-to-do')
 const formEl = document.querySelector('#task-form')
 
-const createTaskHandler = (event) => {
+const taskFormHandler = (event) => {
     event.preventDefault()
 
     const taskNameInput = document.querySelector("input[name='task-name']").value
     const taskTypeInput = document.querySelector("select[name='task-type']").value
-    
-    //Create list item
-    const listItemEl = document.createElement('li')
-    listItemEl.className = 'task-item'
-    
-    //Create div to hold task name/type and add to listItemEl
-    const taskInfoEl = document.createElement('div')
-    taskInfoEl.className = "task-info"
-    
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
 
-    //Append to listItemEl
-    listItemEl.appendChild(taskInfoEl)
+    //task info made into object
+    taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    }
+    //Sending taskDataObj as an argument to createTaskEl()
+    createTaskEl(taskDataObj)
+}
 
-    //Append entire list item to toDolist
-    tasksToDoEl.appendChild(listItemEl)
-    
+const createTaskEl = taskDataObj => {
+      //Create list item
+      const listItemEl = document.createElement('li')
+      listItemEl.className = 'task-item'
+      
+      //Create div to hold task name/type and add to listItemEl
+      const taskInfoEl = document.createElement('div')
+      taskInfoEl.className = "task-info"
+      taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>"
+  
+      //Append to listItemEl
+      listItemEl.appendChild(taskInfoEl)
+  
+      //Append entire list item to toDolist
+      tasksToDoEl.appendChild(listItemEl) 
 
 }
-formEl.addEventListener('submit', createTaskHandler)
+
+formEl.addEventListener('submit', taskFormHandler)
