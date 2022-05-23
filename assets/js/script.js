bcfef26dc304ba86cca3b3b44a31f7b77389ef35
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-05-23 15:06:11 -0400

    added ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const tasksToDoEl = document.querySelector('#tasks-to-do')
 const formEl = document.querySelector('#task-form')
 const pageContentEl = document.querySelector('#page-content')
+const tasksInProgressEl = document.querySelector("#tasks-in-progress")
+const tasksCompletedEl = document.querySelector("tasks-completed")
 let taskIdCounter = 0;
 
-//** Function **/
+//** Function **/ This is where creating the list items begins by using the form and adding new tasks as well as editing tasks
 const taskFormHandler = (event) => {
     event.preventDefault()
 
@@ -17,15 +19,25 @@ const taskFormHandler = (event) => {
     }
     formEl.reset() //resets form inputs to original state(only works on forms) 
 
+    const isEdit = formEl.hasAttribute("data-task-id");
+     //Willl show false if the task does not have a data-task-id (Remember that data-task-id is created when createTaskEl function runs and all consecutive functions, so "false" will show up first for the listitemEl)
+
+    // If formEl has data attribute "data-task-id ", get task id and call completeEditTask()
+    if (isEdit) {
+        const taskId = formEl.getAttribute('data-task-id')
+        completeEditTask(taskNameInput, taskTypeInput, taskId)
+    // If formEl doesn't have data attribute, create object as normal and continue creating normal task 
+    } else {
     //task info made into object
     taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
-    }
-    //Sending taskDataObj as an argument to createTaskEl()
+     }
+     //Sending taskDataObj as an argument to createTaskEl()
     createTaskEl(taskDataObj)
+    }
 }
-//** Function **/
+//** Function **/ This creates the actual task list element
 const createTaskEl = taskDataObj => {
       //Create list item
       const listItemEl = document.createElement('li')
@@ -52,7 +64,7 @@ const createTaskEl = taskDataObj => {
       taskIdCounter++
 
 }
-//** Function **/
+//** Function **/ This is creating the elements for "edit", "delete", "status of task"
 const createTaskActions = taskId => {
     const actionContainerEl = document.createElement('div')
     actionContainerEl.className = 'task-actions'
@@ -96,9 +108,9 @@ const createTaskActions = taskId => {
     return actionContainerEl
 }
 
-//** Function **/
+//** Function **/ Shows and mataches which element on screen is being clicked using the event.target
 const taskButtonHandler = (event) => {
-    const targetEl = event.target // get's target element froom event 
+    const targetEl = event.target // get's target element from event 
     console.log(targetEl) // identifies what container is being clicked
 
     if(targetEl.matches('.edit-btn')){
@@ -110,13 +122,13 @@ const taskButtonHandler = (event) => {
         deleteTask(taskId)
     }
 }
-//** Function **/
+//** Function **/ Deletes task created 
 const deleteTask = taskId => {
     let taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']")
     taskSelected.remove() // remove() is a method that will remove the item from the page 
 }
 
-//** Function **/
+//** Function **/ This function will grab the task made and plug the text content of task Name and task Type back into input fields in the form
 const editTask = taskId => {
     console.log('editing task #' + taskId)
 
@@ -136,11 +148,42 @@ const editTask = taskId => {
     formEl.setAttribute("data-task-id", taskId)
    
 }
+//** Function */ Will finish changing text of task items
+const completeEditTask = (taskName, taskType, taskId) => {
+    // Finding the matching task list item
+    let taskSelected = document.querySelector(".task-item[data-task-id='" + taskId +"']")
+    // Set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName
+    taskSelected.querySelector("span.task-type").textContent = taskType 
 
+    alert("Task Updated!")
 
+    // Resetting the form back to regular formEl process with initial data-task-id
+    formEl.removeAttribute("data-task-id")
+    document.querySelector("#save-task").textContent = "Add Task"
+ 
+}
+//** Function */ Changes task item to other columns
+const taskStatusChangeHandler = (event) => {
+    // getting the task item's id
+    const taskId = event.target.getAttribute("data-task-id")
 
+    // getting the currently selected option's value and convert to lowercase
+    const statusValue = event.target.value.toLowerCase()
 
+    // find the parent task item element based on the id
+    let taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']")
+
+    if (statusValue === "to do"){
+        tasksToDoEl.appendChild(taskSelected)
+    } else if (statusValue === "in progress"){
+        tasksInProgressEl.appendChild(taskSelected)
+    } else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected)
+    }
+}
 
 
 pageContentEl.addEventListener('click', taskButtonHandler)
+pageContentEl.addEventListener('change', taskStatusChangeHandler)
 formEl.addEventListener('submit', taskFormHandler)
